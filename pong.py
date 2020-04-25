f51e0fead2786a3b9741a2de6cc3dd853efd2762
hex-plex <sendhilsom7@gmail.com>
2020-04-25 13:49:16 +0530

    paddle

--- pong.py
+++ pong.py
@@ -1,10 +1,23 @@
 import pygame
+from paddle import Paddle
 pygame.init()
 BLACK = (0,0,0)
 WHITE = (255,255,255)
 size = (700,500)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Pong")
+paddleA = Paddle(WHITE,10, 100)
+paddleA.rect.x = 20
+paddleA.rect.y = 200
+
+paddleB = Paddle(WHITE , 10 , 100)
+paddleB.rect.x = 670
+paddleB.rect.y = 200
+
+all_sprites_list = pygame.sprite.Group()
+
+all_sprites_list.add(paddleA)
+all_sprites_list.add(paddleB)
 
 carryOn = True
 
@@ -14,8 +27,16 @@ while carryOn:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             carryOn = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_x:
+                carryOn = False
+
+    all_sprites_list.update()
+
+
     screen.fill(BLACK)
     pygame.draw.line(screen, WHITE, [349,0], [349,500], 5)
+    all_sprites_list.draw(screen)
     pygame.display.flip()
     clock.tick(60)
 
