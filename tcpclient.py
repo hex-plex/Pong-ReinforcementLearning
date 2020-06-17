5be8b94693e245e5e50958e6b9a48c8d88ca7e75
HexPlex <sendhilsom7@gmail.com>
2020-06-17 15:16:52 +0530

    Socket improvements

--- tcpclient.py
+++ tcpclient.py
@@ -1,11 +1,11 @@
 import socket
 
 def client():
-  host = socket.gethostname()  # get local machine name
-  port = 8080  # Make sure it's within the > 1024 $$ <65535 range
+  #host = socket.gethostname()  # get local machine name
+  port = 18000  # Make sure it's within the > 1024 $$ <65535 range
 
   s = socket.socket()
-  s.connect((host, port))
+  s.connect(("192.168.1.6", port))
 
   message = input('-> ')
   while message != 'q':
--- tcpserver.py
+++ tcpserver.py
@@ -1,22 +1,24 @@
 import socket
 
 def server():
-  host = socket.gethostname()   # get local machine name
-  port = 8080  # Make sure it's within the > 1024 $$ <65535 range
+  #host = socket.gethostname()   # get local machine name
+  port = 18000  # Make sure it's within the > 1024 $$ <65535 range
 
   s = socket.socket()
-  s.bind((host, port))
+  s.bind(('192.168.1.6', port))
 
   s.listen(1)
   client_socket, adress = s.accept()
   print("Connection from: " + str(adress))
   while True:
-    data = s.recv(adress[1]).decode('utf-8')
+    data = client_socket.recv(1024).decode('utf-8')
     if not data:
       break
     print('From online user: ' + data)
     data = data.upper()
-    s.send(data.encode('utf-8'))
+    client_socket.send(data.encode('utf-8'))
+  client_socket.close()
   s.close()
 
-if __name__ == '__main__':server()
+if __name__ == '__main__':
+    server()
