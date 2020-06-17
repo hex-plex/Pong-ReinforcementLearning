f71b85323c8dc71334a691c56ca266560d070434
HexPlex <sendhilsom7@gmail.com>
2020-06-17 21:00:37 +0530

    Socket Model Done

--- mainmodel.py
+++ mainmodel.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pickle
-
+import socket
+from io import BytesIO
+import _thread
 class PolicyGradient:
 	def sigmoid(self,x):
 		return 1.0/(1.0+np.exp(-x))
@@ -29,7 +31,7 @@ class PolicyGradient:
 		dh[self.eph<=0] = 0
 		dW1 = np.dot(dh.T,self.epx)
 		return {'dW1':dW1,'dW2':dW2}
-	def __init__(self,resume=False,render=False,hiddenUnits = 250,batch_size = 10,learningRate= 1e-3,gamma = 0.99,decayRate = 0.99):
+	def __init__(self,resume=False,render=False,hiddenUnits = 250,batch_size = 10,learningRate= 1e-3,gamma = 0.99,decayRate = 0.99,host=None,port=12345):
 		if resume:
 			self.model = pickle.load(open("checkpoint.p",'rb'))
 		else:
@@ -45,23 +47,69 @@ class PolicyGradient:
 		self.decayRate = decayRate
 		self.render = render
 		self.dim = 70*50
-	def connect_env(self):
-		pass
+		self.socket=None
+		self.host = host
+		self.port = port
+		self.handshake = False
+		self.runningFlag=True
+		self.ongoingFlag=True
+	def propriety_control(self,dummy):
+		while self.runningFlag:
+			a=input()
+			if a=='p':
+				print("Pausing the training session" if self.ongoingFlag else "Resuming the training session")
+				self.ongoingFlag= not self.ongoingFlag
+			elif 'q':
+				self.ongoingFlag=False
+				print("Interupting saving the previous checkpoint to interrupt.p .")
+				pickle.dump(self.model, open('interrupt.p', 'wb'))
+				time.sleep(0.02)
+				exit()
+			## If cases to pause the environment
+		## This is to give a basic control over the training and other things
+		return True
+	def connect_env(self,host): ## didnt use try except as to view particular errors
+		if host is None:
+			host = socket.gethostname()
+		self.socket = socket.socket()
+		self.socket.connect((host,self.port))
+		print("Connection established with "+str(host)+" on port "+str(self.port))
+		self.handshake=True
 	def get_frame(self):
-		pass
+		if self.handshake:
+			self.socket.send('r'.encode('utf-8'))
+			img_buffer=''
+			while True:
+				inp_buffer=self.socket.recv(1024)
+				if not inp_buffer: break
+				img_buffer+=inp_buffer
+			return np.load(BytesIO(img_buffer))['frame']
+		else:
+			raise Exception('Connect to the environment first')
+
 	def get_reward(self):
-		pass
-	def send_action(self):
-		pass
+		if self.handshake:
+			self.socket.send('s'.encode('utf-8'))
+			return int(self.socket.recv(1024).decode('utf-8'))
+		else:
+			raise Exception('Connect to the environment first')
+	def send_action(self,action):
+		if self.handshake:
+			self.socket.send(action.encode('utf-8')) ## Add a confirmation
+		else:
+			raise Exception('Connect to the environment first')
 	def start(self):
-		self.connect_env()
+		_thread.start_new_thread(self.connect_env, (self.host,))
+		_thread.start_new_thread(self.propriety_control, (True,))
+		while not self.handshake:
+			sleep.time(0.05)
 		observation = self.get_frame()
 		returns = None
 		prev_x = None
 		xs,hs,dlogs,drs=[],[],[],[]
 		eps_no=0
 		reward_sum=0
-		while True:
+		while self.ongoingFlag:
 			if render:
 				cv2.imshow("OUTPUT DONT Use This!!!",observation)
 				cv2.waitKey(5)
@@ -69,11 +117,12 @@ class PolicyGradient:
 			x = cur_x - prev_x if prev_x is not None else np.zeros(self.dim)
 			prev_x = cur_x
 			aprob, h = self.fc(x)
-			action = 2 if np.random.uniform() < aprob else 3
+			action = 1 if np.random.uniform() < aprob else 2
 			xs.append(x)
 			hs.append(h)
-			y = 1 if action==2 else 0
+			y = 1 if action==1 else 0
 			dlogps.append(y-aprob)
+			self.send_action(str(action))
 			observation = self.get_frame()
 			reward = self.get_reward()
 			done = True if reward!=0 else False
@@ -101,7 +150,8 @@ class PolicyGradient:
 				returns = reward_sum if returns is None else returns*0.99 + reward_sum*0.01
 				print(('This episode' if eps_no==0 else 'Another episode') + ' is completed with total reward: '+str(reward_sum)+' and runnign mean: '+str(returns))
 				if episode_no%100==0:
-					reward_sum=0
+					 pickle.dump(self.model, open('save.p', 'wb'))
+				reward_sum=0
 				time.sleep(0.05)## THis is for the ball to go away from the boundary after someone has won
 				observation = self.get_frame()
 				prev_x=None
